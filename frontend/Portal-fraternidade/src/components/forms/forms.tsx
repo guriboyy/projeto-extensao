fdc489d856290f3bb379caeb4925f3359f098673
Guilherme N <guilherme.evangelista@synergroup.com.br>
2025-11-03 20:08:31 -0300

    Teste commit

--- frontend/Portal-fraternidade/src/components/forms/forms.tsx
+++ frontend/Portal-fraternidade/src/components/forms/forms.tsx
@@ -3,13 +3,14 @@ import Form from 'react-bootstrap/Form';
 
 
 import {useForm} from '../../hooks/useFormHooks'
+import { Link } from 'react-router-dom';
 
-function FormsDefault(text:string) {
+function FormsDefault() {
     const {setEmail, setPasswd,handleForm, error} = useForm();
     return(
      
         <Form onSubmit={(e)=> handleForm(e)} className="p-5 shadow rounded" style={{ width: "80%", maxWidth: "400px" }}>
-            <h3 className="text-center mb-4">{text}</h3>
+            <h3 className="text-center mb-4">Bem vindo</h3>
               <Form.Group  className="mb-3" controlId="formBasicEmail">                
                 <Form.Label>Email address</Form.Label>
                 <Form.Control onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
@@ -22,8 +23,14 @@ function FormsDefault(text:string) {
                 <Form.Label>Password</Form.Label>
                 <Form.Control onChange={(e) => setPasswd(e.target.value)} type="password" placeholder="Password" />
                   <Form.Text id='information' className="text-muted mt-5 ">
-                    {error}
+                     <Link to={'/forgetPassword'}>Esqueci minha senha</Link>
                   </Form.Text>
+               
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                 <Form.Text id='information' className="text-muted mt-5 ">
+                    {error}
+                  </Form.Text>               
               </Form.Group>
               
               <Button className='' variant="primary" type="submit">
