7e1d465836cc0c943dcae6672d25ba41dceb63cb
Gabriel Mattos <gabrielmattosdev@gmail.com>
2025-11-03 22:41:50 -0300

    feat: addint middleware in routes

--- backend/src/dtos/responses/auth/authResponseDTO.ts
+++ backend/src/dtos/responses/auth/authResponseDTO.ts
@@ -1,4 +1,5 @@
 export type authResponseDTO = {
     accessToken: string;
     refreshToken: string;
+    userAccountId: number;
 }
--- backend/src/routes/noticeBoardRoutes.ts
+++ backend/src/routes/noticeBoardRoutes.ts
@@ -1,26 +1,27 @@
 import express from 'express';
 import { NotificationController } from "../controllers/NoticeBoardController";
+import { checkAuthenticated } from '../middleware/middleware';
 
 const router = express.Router();
 const noticeBoardController = new NotificationController();
 
-router.post('/create', (req, res) => {    
+router.post('/create', checkAuthenticated(["Admin"]), (req, res) => {    
     noticeBoardController.create(req,res)
 });
 
-router.get('/get-all', (req, res) => {
+router.get('/get-all', checkAuthenticated(["Admin", "Obreiro", "Membro"]),(req, res) => {
     noticeBoardController.getAll(req, res);
 });
 
-router.get('/get/:noticeBoardId', (req, res) => {
+router.get('/get/:noticeBoardId', checkAuthenticated(["Admin"]), (req, res) => {
     noticeBoardController.getById(req, res);
 });
 
-router.put('/update/:noticeBoardId', (req, res) => {
+router.put('/update/:noticeBoardId', checkAuthenticated(["Admin"]), (req, res) => {
     noticeBoardController.update(req, res);
 });
 
-router.delete('/delete/:noticeBoardId', (req, res) => {
+router.delete('/delete/:noticeBoardId', checkAuthenticated(["Admin"]), (req, res) => {
     noticeBoardController.delete(req, res);
 });
 
